de7ac9393535878f02296505afd7c042339b629b
Tanmoy037 <tanmoysantra67@gmail.com>
2023-04-22 19:00:35 +0530

    fixed

--- main.go
+++ main.go
@@ -1,65 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 func main() {
-	conferenceName := "Go Conference"
-	var conferenceTicktes uint = 50
-	var remainingTicktes uint = 50
-	bookings := []string{}
+    conferenceName := "Go Conference"
+    var conferenceTickets uint = 50
+    var remainingTickets uint = 50
+    bookings := []string{}
 
-	fmt.Printf("Welcome %v booking application\n", conferenceName)
-	fmt.Println("We have total of", conferenceTicktes ,"tickets and", remainingTicktes, "are still available")
-	fmt.Println("Get your ticktes here to attend")
+    fmt.Printf("Welcome %v booking application ", conferenceName )
 
-	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTicket int
-		fmt.Println("Enter first your name: ")
-		fmt.Scan(&firstName)
-		fmt.Println("Enter last name: ")
-		fmt.Scan(&lastName)
-		fmt.Println("Enter your email: ")
-		fmt.Scan(&email)
-		fmt.Println("Enter number of tickets: ")
-		fmt.Scan(&userTicket)
+    fmt.Println("We have total of", conferenceTickets ,"tickets and", remainingTickets, "are still available")
+    fmt.Println("Get your tickets here to attend")
 
+    for {
+        var firstName string
+        var lastName string
+        var email string
+        var userTicket int
+        fmt.Println("Enter your first name: ")
+        fmt.Scan(&firstName)
+        fmt.Println("Enter your last name: ")
+        fmt.Scan(&lastName)
+        fmt.Println("Enter your email: ")
+        fmt.Scan(&email)
+        fmt.Println("Enter number of tickets: ")
+        _, err := fmt.Scan(&userTicket)
+        if err != nil {
+            fmt.Printf("Invalid input for number of tickets: %v ", err)
+            continue
+        }
+        if userTicket < 0 {
+            fmt.Println("You cannot book a negative number of tickets")
+            continue
+        }
 
-		remainingTicktes = remainingTicktes - uint(userTicket)
-		bookings = append(bookings, firstName + " " + lastName)
+        if uint(userTicket) > remainingTickets {
+            fmt.Printf("We only have %v tickets remaining. You cannot book %v tickets. \n", remainingTickets, userTicket)
+            continue
+        }
 
-		if userTicket > int(remainingTicktes) {
-			fmt.Printf("We only have %v tickets remaining. So you can't book %v tickets. \n", remainingTicktes, userTicket)
-			continue
-			
-		}
+        remainingTickets -= uint(userTicket)
+        bookings = append(bookings, firstName + " " + lastName)
 
+        fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTicket, email)
+        fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTicket, email)
-		fmt.Printf("%v tickets remaining for %v \n", remainingTicktes, conferenceName)
+        firstNames := []string{}
+        for _, booking := range bookings {
+            names := strings.Fields(booking)
+            firstNames = append(firstNames, names[0])
+        }
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+        fmt.Printf("These first names of bookings are: %v ", firstNames)
 
-		}
-
-
-		fmt.Printf("These first names of bookings are: %v\n", firstName )
-
-
-		if remainingTicktes == 0 { 
-			
-			fmt.Println("Our conference is booked out. Come back next year.")
-			
-			break
-		}
-	}
-
+        if remainingTickets == 0 {
+            fmt.Println("Our conference is booked out. Come back next year.")
+            break
+        }
+    }
 }
